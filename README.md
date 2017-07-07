213a25f2e9637a060d973a4a4259a765bb50988a
Kano Doe <bad_cholo@yahoo.com>
2017-07-07 16:32:00 -0700

    Badcholos update 1

--- README.md
+++
@@ -1,6 +0,0 @@
-# Inventory-S
-
-Hello everyone!
-
-This is a simple file being used for commit changes.
-Future changes will be made.
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
